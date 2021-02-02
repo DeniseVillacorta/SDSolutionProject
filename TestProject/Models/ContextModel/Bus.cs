30399e49e0b83040b23ceb1d0de6460f867446b2
Denise Villacorta <villacorta9806denise@gmail.com>
2021-02-02 16:00:17 +0800

    Added Index and Form for Bus

--- TestProject/Models/ContextModel/Bus.cs
+++ TestProject/Models/ContextModel/Bus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,15 @@ namespace TestProject.Models.ContextModel
     public class Bus
     {
         public int Id { get; set; }
+
+        [Display(Name = "Bus Number")]
         public int BusNo { get; set; }
+
+        [Display(Name = "Plate Number")]
         public int PlateNo { get; set; }
         public Employee Employee { get; set; }
+
+        [Display(Name = "Employee Driver")]
         public int EmployeeId { get; set; }
     }
 }
