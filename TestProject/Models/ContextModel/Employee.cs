b6ac7e648b96272cac599af6fa79f8742500391b
Denise Villacorta <villacorta9806denise@gmail.com>
2021-02-02 15:48:58 +0800

    Added Index and Form for Employee

--- TestProject/Models/ContextModel/Employee.cs
+++ TestProject/Models/ContextModel/Employee.cs
@@ -9,9 +9,17 @@ namespace TestProject.Models.ContextModel
     public class Employee
     {
         public int Id { get; set; }
+
+        [Display (Name ="First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Employee Number")]
         public int EmployeeNo { get; set; }
+
+        [Display(Name = "Contact Number")]
         public int ContactNo { get; set; }
     }
 }
