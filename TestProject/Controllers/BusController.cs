652526b072d273dc8bc432748429449d2e254899
Denise Villacorta <villacorta9806denise@gmail.com>
2021-02-02 14:59:17 +0800

    Added Bus and Employee Controller

---
+++ TestProject/Controllers/BusController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    public class BusController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public BusController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Bus
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestProject/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EmployeeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
