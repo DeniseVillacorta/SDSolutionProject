ee25d23487f475948eeb3351d7815e15977f7b1a
Denise Villacorta <villacorta9806denise@gmail.com>
2021-02-02 15:25:53 +0800

    Added New, Edit and Delete Action in Employee Controller

--- TestProject/Controllers/EmployeeController.cs
+++ TestProject/Controllers/EmployeeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TestProject.Models;
+using TestProject.Models.ContextModel;
+using TestProject.Models.ViewModel.Employees;
 
 namespace TestProject.Controllers
 {
@@ -25,6 +27,43 @@ namespace TestProject.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult New()
+        {
+            var viewModel = new EmployeeFormViewModel
+            {
+                Employee = new Employee()
+            };
+
+            return View("EmployeeForm", viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var employee = _context.Employees.SingleOrDefault(e => e.Id == id);
+
+            if (employee == null)
+                return HttpNotFound();
+
+            var viewModel = new EmployeeFormViewModel
+            {
+                Employee = employee,
+            };
+
+            return View("EmployeeForm", viewModel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var employee = _context.Employees.SingleOrDefault(e => e.Id == id);
+
+            if (employee == null)
+                return HttpNotFound();
+
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Employee");
         }
     }
 }
